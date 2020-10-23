523ccb8a021827aed2e094495e1ffded44672a3c
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-23 20:09:02 +0200

    añadido gitignore

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    module : {
+        rules: [
+
+        ]
+    }
+};
