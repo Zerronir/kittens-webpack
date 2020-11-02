c037a93fa6b1a745ceda9a6cfcf877d450629e4e
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-11-02 22:50:19 +0100

    Proyecto finalizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,52 @@
-const path = require('path');
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    module : {
-        rules: [
-
+    entry: {
+        vendor: [
+            './src/js/application.js',
+            './src/js/jquery-2.1.0.min.js',
+            './src/js/modernizr-2.5.2.min.js'
         ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: './src/js/jquery-2.1.0.min.js'
+        }),
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template: './src/index.html'
+        }),
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+                exclude: '/node_modules/'
+            },
+        ]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, './dist/'),
+    },
+    externals: {
+        jquery: 'jQuery'
     }
 };
